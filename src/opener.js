92f4a9ea9ecc3a7d98894d00cb614e95ca9f800e
spectral256 <_>
2020-07-26 08:36:12 +0200

    changed to work with link and selection

--- src/opener.js
+++ src/opener.js
@@ -1,13 +1,20 @@
 browser.menus.create({
     id: "sci-hub-open",
     title: "Open in Sci-Hub",
-    contexts: ["selection"]
+    contexts: ["link", "selection"]
 });
 
 browser.menus.onClicked.addListener(function(info, tab) {
     switch (info.menuItemId) {
       case "sci-hub-open":
-        var url = `https://sci-hub.tw/${info.selectionText}`;
+          var path;
+          if(info.hasOwnProperty("linkUrl")){
+              path = info.linkUrl;
+          }
+          else if(info.hasOwnProperty("selectionText")){
+              path = info.selectionText;
+          }
+        var url = `https://sci-hub.tw/${path}`;
         createTab(url);
     }
   });
